a68155830a64bdfe61abb0bf23ec51a09d6d8b4a
bombiswine <60137683+bombiswine@users.noreply.github.com>
2020-10-24 23:01:04 +0600

    Update Merson_2_3.c

--- Merson_2_3.c
+++ Merson_2_3.c
@@ -1,12 +1,12 @@
 /*
 	Author: Merson Alexandr
 
-	Group: СМБ-001-O-01
+	Group: Г‘ГЊГЃ-001-O-01
 
 	Task#: 2.3
 
-	Description: Счётчик отрицательных чисел
-				 в вводимой последовательности.
+	Description: Г‘Г·ВёГІГ·ГЁГЄ Г®ГІГ°ГЁГ¶Г ГІГҐГ«ГјГ­Г»Гµ Г·ГЁГ±ГҐГ«
+				 Гў ГўГўГ®Г¤ГЁГ¬Г®Г© ГЇГ®Г±Г«ГҐГ¤Г®ГўГ ГІГҐГ«ГјГ­Г®Г±ГІГЁ.
 */
 
 #include <stdio.h>
@@ -21,18 +21,18 @@ int main()
 	printf("\n This program counts the number of negative numbers in the sequence"
 		   "\n If you want to end the program enter 0 \n");
 	count = 0;
-	while(element != 0) {
+	while (element != 0) {
 		printf(" Enter the element of sequence: ");
-        do {
-            input_var_amount = scanf("%lf", &element);
-            input_value_is_number = input_var_amount == 1;
+		do {
+		    input_var_amount = scanf("%lf", &element);
+		    input_value_is_number = input_var_amount == 1;
 
-            if (!input_value_is_number) {
-                printf("\n Input error: input value must be number!");
-                fflush(stdin);
-                printf("\n Please enter the element of sequence correctly: ");
-            }
-        } while (!input_value_is_number);
+		    if (!input_value_is_number) {
+			printf("\n Input error: input value must be number!");
+			fflush(stdin);
+			printf("\n Please enter the element of sequence correctly: ");
+		    }
+		} while (!input_value_is_number);
 
 		if (element < 0) { count++; }
 	}
