d5116d3e86020e278fa9bccdd1b25f9d224ffd81
bombiswine <60137683+bombiswine@users.noreply.github.com>
2020-10-24 22:59:08 +0600

    Update Merson_1_.c

--- Merson_1_.c
+++ Merson_1_.c
@@ -15,9 +15,10 @@ int main()
     double lengthCm, lengthM, lengthKm;
     printf("\n This program converts the length entered in cm to m and km");
     //проверка корректности введённого пользователем значения
-    // алтернатива логическому типу
+    //алтернатива логическому типу
     int input_var_amont = 0;
     int input_value_is_integer = 0;
+    
     do {
         printf("\n Please enter the length in centimeters: ");
         input_var_amont = scanf("%lf", &lengthCm );
@@ -27,6 +28,7 @@ int main()
             fflush(stdin); //ñáðîñ áóôôåðà ââîäà
         }
     } while(!input_value_is_integer);
+    
     lengthM = lengthCm / 100;
     lengthKm = lengthM / 1000;
     printf("\n Length in meters: %f \n Length in kilometers: %f \n", lengthM, lengthKm);
